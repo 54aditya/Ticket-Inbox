3856ee2537f163d05342c5da1a3a0082226f1307
54aditya <145246136+54aditya@users.noreply.github.com>
2025-12-31 03:34:00 +0530

    Update database.js

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -10,12 +10,9 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: [User, Ticket, Note],
   synchronize: true,
-  logging: false,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
+
